532cf28f5c025b99cd426cc50036357bf582a784
Shawn Stolsig <shawnstolsig@gmail.com>
2021-05-09 15:27:37 -0700

    landing page and fixed tailwind config

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
-import Layout from '../components/layout'
 import '../styles/index.css'
+
+// import Layout from '../components/layout'
+//
+// function MyApp({Component, pageProps}) {
+//     return (
+//         <Layout>
+//             <Component {...pageProps} />
+//         </Layout>
+//     )
+// }
 
 function MyApp({Component, pageProps}) {
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Component {...pageProps} />
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import clsx from "clsx";
 
 import Head from 'next/head'
-
-import Card from '../components/card';
+import Image from 'next/image'
+import logo from '../public/pippi.svg'
 
 export default function Home() {
     return (
@@ -12,23 +12,13 @@ export default function Home() {
                 <title>SV Hoptoad | Home</title>
             </Head>
 
-            <div className={'flex'}>
-
-            <Card>
-                <Card.Title>Card</Card.Title>
-                <Card.Content>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Content>
-                <Card.Actions>
-                    <button onClick={() => alert('hello')}>Click me</button>
-                </Card.Actions>
-            </Card>
-
-            <Card>
-                <Card.Title>Card</Card.Title>
-                <Card.Content>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Card.Content>
-                <Card.Actions>
-                    <button onClick={() => alert('hello')}>Click me</button>
-                </Card.Actions>
-            </Card>
+            <div className={clsx(
+                'flex w-screen h-screen justify-center items-center flex-col',
+                'bg-gradient-to-bl from-gray-300 via-blue-300 to-gray-400',
+            )}>
+                <Image src={logo} height={100} width={200} />
+                <h3 className={'text-3xl font-cooperBlack'}>svhoptoad.com</h3>
+                <h5 className={'text-gray-500 text-lg'}>coming soon</h5>
             </div>
 
         </>
