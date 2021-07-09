1f82b0942e5ae911f804c46ecef4db00363e77c1
Shawn Stolsig <shawnstolsig@gmail.com>
2021-07-08 22:10:24 -0700

    coming soon

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,19 @@
 import '../styles/index.css'
 
-import Layout from '../components/layout'
-function MyApp({Component, pageProps}) {
-    return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
-    )
-}
-
-// import ComingSoon from './comingSoon'                // uncomment below for 'Coming Soon' screen
+// import Layout from '../components/layout'
 // function MyApp({Component, pageProps}) {
 //     return (
-//         <ComingSoon />
+//         <Layout>
+//             <Component {...pageProps} />
+//         </Layout>
 //     )
 // }
+
+import ComingSoon from './comingSoon'                // uncomment below for 'Coming Soon' screen
+function MyApp({Component, pageProps}) {
+    return (
+        <ComingSoon />
+    )
+}
 
 export default MyApp;
