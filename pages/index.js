c358e5392002e8e278b9ca0f3854a0bfee964949
Shawn Stolsig <shawnstolsig@gmail.com>
2021-07-18 23:15:45 -0700

    started working home page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,9 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import Image from "next/image";
+
+import {
+    intro
+} from '../content/home'
 
 export default function Home() {
     return (
@@ -8,8 +13,28 @@ export default function Home() {
                 <title>SV Hoptoad | Home</title>
             </Head>
 
-            <div>
-                Home page
+            <div className={`page flex flex-col items-center`}>
+                <div className={`flex justify-center items-center`}>
+                    <Image
+                        src={intro.sonnyAndMargiePhoto}
+                        layout={`intrinsic`}
+                        width={430}
+                        height={284}
+                        className={`rounded text-center shadow-xl`}
+                    />
+                    <div className={`p-4 w-80`}>
+                        <p>
+                            {intro.para1}
+                        </p>
+                        <br/>
+                        <p>
+                            {intro.para2}
+                        </p>
+                    </div>
+                </div>
+                <blockquote className={`text-2xl italic font-light bg-cyan-100 rounded p-4 w-2/3 text-center shadow-lg`}>
+                    {intro.title}
+                </blockquote>
             </div>
 
         </>
