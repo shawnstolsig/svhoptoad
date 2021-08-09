0b1f63b483a392dc2100ff14820a33f909067581
Shawn Stolsig <shawnstolsig@gmail.com>
2021-08-08 18:14:49 -0700

    added boat selector

---
+++ content/boat.js
@@ -0,0 +1,37 @@
+export const boats = [
+    {
+        name: `Hoptoad`,
+        design: `Moody 46`,
+        href: `moody-46`,
+        length: 46,
+        beam: 12,
+        draft: 7,
+        speed: {
+            max: 9,
+            cruising: 6
+        },
+        material: `Fiberglass`,
+        rig: `Ketch`,
+        years: {
+            built: 1976,
+            start: 2004,
+            end: `Present`
+        },
+        description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris quis nibh vehicula, fermentum sapien id, tempor diam. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean ultricies, orci non semper gravida, neque libero vestibulum dolor, at ultrices quam velit nec diam. Morbi mattis molestie nisi, vitae placerat est imperdiet in. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse bibendum, sem eget eleifend convallis, lacus risus finibus ipsum, ut elementum enim metus ut libero. Morbi non tristique ante, nec molestie felis. Donec luctus quis arcu non ullamcorper. Fusce id ante at diam dapibus rutrum eu eget mauris. Vestibulum viverra mi vitae posuere laoreet. Mauris id libero luctus lorem bibendum dignissim et quis purus. Suspendisse aliquam quis turpis non euismod. Nullam egestas ornare eros id eleifend. Pellentesque mi augue, condimentum et odio eleifend, porttitor pulvinar velit. Vivamus non dui purus.\nCurabitur finibus facilisis suscipit. Sed ut magna arcu. Aenean ultricies, velit at gravida sollicitudin, augue arcu ultrices mi, sit amet molestie neque elit id arcu. Vestibulum accumsan dolor mi, vulputate sagittis sapien suscipit vitae. Curabitur in augue nisi. Curabitur pretium, est at dignissim eleifend, sapien mauris cursus enim, id blandit urna dui quis mi. Sed volutpat ornare nunc, accumsan tristique dolor eleifend at. Nunc tincidunt rhoncus quam, at convallis enim tempor quis. Pellentesque scelerisque elementum neque, eget malesuada sapien iaculis ac. Etiam fermentum finibus diam sed pulvinar.`
+    },
+    {
+        name: `Hoptoad`,
+        design: `Roberts 38`,
+        href: `roberts-38`,
+    },
+    {
+        name: `Hoptoad`,
+        design: `San Juan 30`,
+        href: `san-juan-30`,
+    },
+    {
+        name: `Air Freight`,
+        design: `San Juan 23`,
+        href: `san-juan-23`,
+    },
+]
--- pages/boat/index.js
+++ pages/boat/index.js
@@ -1,14 +1,72 @@
-import Head from "next/head";
+import Head from "next/head"
+import { useRouter } from 'next/router'
+import clsx from "clsx"
+import { useState } from 'react'
+
+import { boats } from "../../content/boat"
+
+function BoatSelector({ boat, setBoat }) {
+    // const router = useRouter()
+
+    return (
+        <div className={'max-w-5xl mx-auto'}>
+            <div className="sm:hidden">
+                <label htmlFor="boat-tabs" className="sr-only">
+                    Select a tab
+                </label>
+                <select
+                    id="boat-tabs"
+                    name="boat-tabs"
+                    className="block w-3/4 focus:ring-cyan-500 focus:border-cyan-500 border-gray-300 rounded-md mx-auto"
+                    onChange={(event) => setBoat(event.target.value)}
+                    value={boat}
+                >
+                    {boats.map(({design, href}) => (
+                        <option key={design} value={href}>{design}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="hidden sm:block">
+                <nav className="relative z-0 rounded-lg shadow flex divide-x divide-gray-200" aria-label="Boat Tabs">
+                    {boats.map(({design, href}, tabIdx) => (
+                        <button
+                            key={design}
+                            // href={`${router.pathname}?boat=${href}`}
+                            onClick={() => setBoat(href)}
+                            className={clsx(
+                                href === boat ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                                tabIdx === 0 ? 'rounded-l-lg' : '',
+                                tabIdx === boats.length - 1 ? 'rounded-r-lg' : '',
+                                'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
+                            )}
+                            aria-current={href === boat ? 'page' : undefined}
+                        >
+                            <span>{design}</span>
+                            <span
+                                aria-hidden="true"
+                                className={clsx(
+                                    href === boat ? 'bg-cyan-500' : 'bg-transparent',
+                                    'absolute inset-x-0 bottom-0 h-0.5'
+                                )}
+                            />
+                        </button>
+                    ))}
+                </nav>
+            </div>
+        </div>
+    )
+}
 
 const Boat = () => {
+    const router = useRouter()
+    const [boat, setBoat] = useState(router.query.boat ? router.query.boat : boats[0].href)
     return (
         <>
             <Head>
                 <title>SV Hoptoad | Boat</title>
             </Head>
-            <div>
-                Boat content
-            </div>
+            <BoatSelector boat={boat} setBoat={setBoat} />
+            {boat}
         </>
     );
 }
