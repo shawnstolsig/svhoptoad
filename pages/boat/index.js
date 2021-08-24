6aed2b2e7e666a53bf187f04356755dc8a11178d
Shawn Stolsig <shawnstolsig@gmail.com>
2021-08-23 23:27:07 -0700

    fixed boat selector on mobile

--- pages/boat/index.js
+++ pages/boat/index.js
@@ -18,50 +18,32 @@ function BoatSelector({ boat, setBoat }) {
     // const router = useRouter()
 
     return (
-        <div className={'max-w-5xl mx-auto'}>
-            {/*<div className="sm:hidden">*/}
-            {/*    <label htmlFor="boat-tabs" className="sr-only">*/}
-            {/*        Select a tab*/}
-            {/*    </label>*/}
-            {/*    <select*/}
-            {/*        id="boat-tabs"*/}
-            {/*        name="boat-tabs"*/}
-            {/*        className="block w-3/4 focus:ring-cyan-500 focus:border-cyan-500 border-gray-300 rounded-md mx-auto"*/}
-            {/*        onChange={(event) => setBoat(event.target.value)}*/}
-            {/*        value={boat}*/}
-            {/*    >*/}
-            {/*        {boats.map(({design, href}) => (*/}
-            {/*            <option key={design} value={href}>{design}</option>*/}
-            {/*        ))}*/}
-            {/*    </select>*/}
-            {/*</div>*/}
-            <div>
-                <nav className="relative z-0 lg:rounded-lg shadow flex divide-x divide-gray-200" aria-label="Boat Tabs">
-                    {boats.map(({design, href}, tabIdx) => (
-                        <button
-                            key={design}
-                            // href={`${router.pathname}?boat=${href}`}
-                            onClick={() => setBoat(href)}
+        <div className={'w-full lg:max-w-4xl mx-auto'}>
+            <nav className="relative z-0 lg:rounded-lg shadow flex divide-x divide-gray-200" aria-label="Boat Tabs">
+                {boats.map(({design, href}, tabIdx) => (
+                    <button
+                        key={design}
+                        // href={`${router.pathname}?boat=${href}`}
+                        onClick={() => setBoat(href)}
+                        className={clsx(
+                            href === boat ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
+                            tabIdx === 0 ? 'lg:rounded-l-lg' : '',
+                            tabIdx === boats.length - 1 ? 'lg:rounded-r-lg' : '',
+                            'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-xs sm:text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
+                        )}
+                        aria-current={href === boat ? 'page' : undefined}
+                    >
+                        <span>{design}</span>
+                        <span
+                            aria-hidden="true"
                             className={clsx(
-                                href === boat ? 'text-gray-900' : 'text-gray-500 hover:text-gray-700',
-                                tabIdx === 0 ? 'rounded-l-lg' : '',
-                                tabIdx === boats.length - 1 ? 'rounded-r-lg' : '',
-                                'group relative min-w-0 flex-1 overflow-hidden bg-white py-4 px-4 text-sm font-medium text-center hover:bg-gray-50 focus:z-10'
+                                href === boat ? 'bg-cyan-500' : 'bg-transparent',
+                                'absolute inset-x-0 bottom-0 h-0.5'
                             )}
-                            aria-current={href === boat ? 'page' : undefined}
-                        >
-                            <span>{design}</span>
-                            <span
-                                aria-hidden="true"
-                                className={clsx(
-                                    href === boat ? 'bg-cyan-500' : 'bg-transparent',
-                                    'absolute inset-x-0 bottom-0 h-0.5'
-                                )}
-                            />
-                        </button>
-                    ))}
-                </nav>
-            </div>
+                        />
+                    </button>
+                ))}
+            </nav>
         </div>
     )
 }
@@ -157,8 +139,8 @@ function BoatGear({gear}){
                                         </span>
                                     </Disclosure.Button>
                                 </h3>
-                                <Disclosure.Panel as="div" className={"py-2 mx-auto"}>
-                                    <div className="flex justify-center">
+                                <Disclosure.Panel as="div" className={"mx-auto"}>
+                                    <div className="flex justify-center p-2">
                                        <Image src={image} className={'rounded'} height={300} width={300} objectFit={'contain'}/>
                                         <div className={'flex flex-col justify-center ml-4'}>
                                             <h4 className="text-xl font-bold mt-0">{name}</h4>
