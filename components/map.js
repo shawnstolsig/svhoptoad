945259b55de94e7af15f4ababa55c1f2e1af6b39
Shawn Stolsig <shawnstolsig@gmail.com>
2021-11-07 15:12:14 -0800

    refactoring to make blog post cards more dynamic

--- components/map.js
+++ components/map.js
@@ -2,14 +2,12 @@ import mapboxgl from 'mapbox-gl'
 import { useRef, useState, useEffect } from 'react'
 import { PlusIcon as PlusIconSolid, MinusIcon as MinusIconSolid } from '@heroicons/react/solid'
 
-import circumnavigation from '../content/circumnavigation.json'
-
-function Map(){
+export function TripMap({initLng, initLat, initZoom, route}){
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-106.5);
-    const [lat, setLat] = useState(23.2);
-    const [zoom, setZoom] = useState(.2);
+    const [lng, setLng] = useState(initLng);
+    const [lat, setLat] = useState(initLat);
+    const [zoom, setZoom] = useState(initZoom);
 
     mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
 
@@ -24,7 +22,7 @@ function Map(){
         });
         // add circumnavigation path
         map.current.on('load', () => {
-            map.current.addSource('route', circumnavigation );
+            map.current.addSource('route', route );
             map.current.addLayer({
                 'id': 'route',
                 'type': 'line',
@@ -89,4 +87,83 @@ function Map(){
     );
 }
 
-export default Map
+export function PinMap({pinLng, pinLat, initZoom}){
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(pinLng);
+    const [lat, setLat] = useState(pinLat);
+    const [zoom, setZoom] = useState(initZoom);
+
+    mapboxgl.accessToken = process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [lng, lat],
+            zoom: zoom,
+            attributionControl: false
+        });
+        // add circumnavigation path
+        map.current.on('load', () => {
+            map.current.addSource('pinSource', {
+                "type": "geojson",
+                "data": {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": [pinLat, pinLng]
+                    },
+                    "properties": {
+                        "title": "Mapbox DC",
+                        "marker-symbol": "monument"
+                    }
+                }
+            });
+
+        });
+    });
+
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+            setLng(map.current.getCenter().lng.toFixed(2));
+            setLat(map.current.getCenter().lat.toFixed(2));
+            setZoom(Number(map.current.getZoom().toFixed(2)));
+        });
+    });
+
+    return (
+        <div className={'relative'}>
+
+            {/*Zoom buttons*/}
+            <div className={"px-1 py-1 z-10 absolute bottom-0 right-0 m-4 rounded text-xs"}>
+                <button
+                    type="button"
+                    className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500 mr-2"
+                    onClick={() => {
+                        map.current.setZoom(zoom + 1)
+                        setZoom(zoom + 1 > 20 ? 20: zoom + 1)
+                    }}
+                >
+                    <PlusIconSolid className="h-5 w-5" aria-hidden="true" />
+                </button>
+                <button
+                    type="button"
+                    className="inline-flex items-center p-1 border border-transparent rounded-full shadow-sm text-white bg-cyan-600 hover:bg-cyan-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-cyan-500"
+                    onClick={() => {
+                        map.current.setZoom(zoom - 1)
+                        setZoom(zoom - 1 < 0 ? 0: zoom - 1)
+                    }}
+                >
+                    <MinusIconSolid className="h-5 w-5" aria-hidden="true" />
+                </button>
+            </div>
+
+            {/*Map*/}
+            <div ref={mapContainer} className={"h-80"}/>
+
+        </div>
+    );
+}
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,12 +1,21 @@
 import Head from "next/head";
 
-import {
-    blog
-} from '../../content/blog'
+import {blog} from '../../content/blog'
+import {formatPredictWindPosts} from "../../util";
+import {PinMap} from "../../components/map";
 
 const Blog = (props) => {
-    const { title, subtitle, samplePosts } = blog
+    const { title, subtitle } = blog
     const { predictWindPosts } = props
+
+    // re-structure/format Predict Wind blog posts
+    const formattedPredictWindsPosts = formatPredictWindPosts(predictWindPosts)
+
+    // combine formatted posts from different sources together
+    const posts = formattedPredictWindsPosts
+
+    // sort by recent - oldest
+    posts.sort((a,b) => b.date - a.date)
 
     return (
         <>
@@ -15,11 +24,6 @@ const Blog = (props) => {
             </Head>
 
             <div className="relative pt-4 pb-6 px-4 sm:px-6 lg:pt-10 lg:pb-10 lg:px-8">
-
-                {/*removing for now...not sure the point of this div*/}
-                {/*<div className="absolute inset-0">*/}
-                {/*    <div className="bg-white h-1/3 sm:h-2/3" />*/}
-                {/*</div>*/}
 
                 <div className="relative max-w-7xl mx-auto">
 
@@ -35,19 +39,10 @@ const Blog = (props) => {
                     <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
 
                         {/*Cards: title, imageUrl, category.href, category.name, description, datetime, date*/}
-                        {predictWindPosts.map(post => {
-                            const {
-                                topic_id: id,
-                                title,
-                                raw: content,
-                                created_at: date,
-                                cooked: htmlContent
-                            } = post
+                        {posts.map(({card}) => {
 
                             /**
                              * BLOG TODO :
-                             * move post data conditioning/naming out of map, so that posts from different sources can be combined before iterating cards
-                             * strip off images from htmlContent
                              * create little thumbnail maps for posts without pictures
                              * add spaces between <p> tags in cooked
                              * fix the card sizes, show popup if larger than card
@@ -55,80 +50,7 @@ const Blog = (props) => {
                              * 1SE videos?
                              */
 
-                                // pull out any image URL
-                            let cardImageUrl
-                            const photoRegex = /!\[Photo\|\d+x\d+]\((.+)\)/g;
-                            const photoMatches = content.match(photoRegex)
-                            if(photoMatches){
-                                const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
-                                const url = photoMatches[0].match(urlRegex)
-                                if(url){
-                                    cardImageUrl = url[0].substring(0, url[0].length - 1)  // chop off trailing ')'
-                                }
-                            }
-
-                            return (
-                                <div key={id} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
-
-                                    {/*Image*/}
-                                    <div className="flex-shrink-0">
-                                        <img className="h-48 w-full object-cover" src={cardImageUrl} alt=""/>
-                                    </div>
-
-                                    {/*Card content*/}
-                                    <div className="flex-1 bg-white p-6 flex flex-col justify-between">
-
-
-                                        <div className="flex-1">
-
-                                            {/*Post type*/}
-                                            <p className="text-sm font-medium text-cyan-600">
-                                                {/*<a href={post.category.href} className="hover:underline">*/}
-                                                {/*    {post.category.name}*/}
-                                                {/*</a>*/}
-                                                Satellite Update
-                                            </p>
-
-                                            {/*Post title and text content*/}
-                                            <a href={'#'} className="block mt-2">
-                                                <p className="text-xl font-semibold text-gray-900">{title}</p>
-                                                {/*<p className="mt-3 text-base text-gray-500">{strippedContent}</p>*/}
-                                                <p className="mt-3 text-base text-gray-500" dangerouslySetInnerHTML={{ __html: htmlContent }} />
-                                            </a>
-                                        </div>
-
-                                        {/*Post date*/}
-                                        <div className="flex space-x-1 text-sm text-gray-500 mt-6">
-                                            <time
-                                                dateTime={new Date(date).toDateString()}>{new Date(date).toLocaleString()}</time>
-                                        </div>
-
-                                        {/*author section, not using*/}
-                                        {/*<div className="mt-6 flex items-center">*/}
-                                        {/*    <div className="flex-shrink-0">*/}
-                                        {/*        <a href={post.author.href}>*/}
-                                        {/*            <span className="sr-only">{post.author.name}</span>*/}
-                                        {/*            <img className="h-10 w-10 rounded-full" src={post.author.imageUrl} alt="" />*/}
-                                        {/*        </a>*/}
-                                        {/*    </div>*/}
-                                        {/*    <div className="ml-3">*/}
-                                        {/*        <p className="text-sm font-medium text-gray-900">*/}
-                                        {/*            <a href={post.author.href} className="hover:underline">*/}
-                                        {/*                {post.author.name}*/}
-                                        {/*            </a>*/}
-                                        {/*        </p>*/}
-                                        {/*        <div className="flex space-x-1 text-sm text-gray-500">*/}
-                                        {/*            <time dateTime={post.datetime}>{post.date}</time>*/}
-                                        {/*            <span aria-hidden="true">&middot;</span>*/}
-                                        {/*            <span>{post.readingTime} read</span>*/}
-                                        {/*        </div>*/}
-                                        {/*    </div>*/}
-                                        {/*</div>*/}
-
-
-                                    </div>
-                                </div>
-                            )
+                            return card
                         })}
                     </div>
 
