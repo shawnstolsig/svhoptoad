2905e359b845a9caf0a323574f3a0504f0971e0d
Shawn Stolsig <shawnstolsig@gmail.com>
2021-11-07 19:01:24 -0800

    working on blog post styling

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -70,7 +70,7 @@ const Blog = (props) => {
                                             {/*Post title and text content*/}
                                             <a href={'#'} className="block mt-2">
                                                 <p className="text-xl font-semibold text-gray-900">{title}</p>
-                                                <p className="mt-3 text-base text-gray-500 h-80 overflow-y-scroll border" dangerouslySetInnerHTML={{__html: content}}/>
+                                                <p className="mt-3 text-base text-gray-500 max-h-80 overflow-y-scroll border" dangerouslySetInnerHTML={{__html: content}}/>
                                             </a>
                                         </div>
 
