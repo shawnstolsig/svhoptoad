4db0ce107619bd1d81a56c745b91a9d18331de3a
Shawn Stolsig <shawnstolsig@gmail.com>
2021-12-09 00:04:45 -0800

    added date picker

---
+++ components/loader.js
@@ -0,0 +1,9 @@
+const Loader = () => {
+    return (
+        <div className="flex items-center justify-center my-12">
+            <div className="w-16 h-16 border-b-2 border-cyan-600 rounded-full animate-spin"></div>
+        </div>
+    );
+}
+
+export default Loader;
