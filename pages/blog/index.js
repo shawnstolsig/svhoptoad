ef836d8eeb247ede21a76d5dbc0d3d594d56c24e
Shawn Stolsig <shawnstolsig@gmail.com>
2023-01-07 22:26:24 -0800

    fixing blog

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -44,143 +44,10 @@ const MonthPicker = ({endDate, setPw, setDateRange, maxDate}) => {
 
 const Blog = ({blogPosts}) => {
 
+    console.log(blogPosts)
+    console.log(blog)
 
-    const { title, subtitle, oneSecondEverydayVideos } = {
-        title: `The blog`,
-        subtitle: `A collection of blog posts from our satellite-based tracking system, monthly recap videos, and more`,
-        oneSecondEverydayVideos: [
-            {
-                key: 1,
-                date: new Date('2021-10-1 00:00:00'),
-                title: 'One Second Everyday: September 2021',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1638945455/blog/one-second-everyday/sep-2021_oim5dd.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 2,
-                date: new Date('2021-11-1 00:00:00'),
-                title: 'One Second Everyday: October 2021',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1636433247/blog/one-second-everyday/oct-2021_r3bnu9.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 3,
-                date: new Date('2021-12-1 00:00:00'),
-                title: 'One Second Everyday: November 2021',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1647304657/blog/one-second-everyday/nov-2021_tgebda.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 4,
-                date: new Date('2022-1-1 00:00:00'),
-                title: 'One Second Everyday: December 2021',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1647304672/blog/one-second-everyday/dec-2021_udfqcl.mp4',
-                type: 'One Second Everyday'
-            },
-            // {
-            //     key: 5,
-            //     date: new Date('2022-2-1 00:00:00'),
-            //     title: 'One Second Everyday: January 2022',
-            //     videoContent: '',
-            //     type: 'One Second Everyday'
-            // },
-            {
-                key: 6,
-                date: new Date('2022-3-1 00:00:00'),
-                title: 'One Second Everyday: February 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1647305411/blog/one-second-everyday/feb-2021_kcsnfz.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 7,
-                date: new Date('2022-4-1 00:00:00'),
-                title: 'One Second Everyday: March 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565871/blog/one-second-everyday/mar-2022_dbwjvl.mp4',
-                type: 'One Second Everyday'
-            },
-            // {
-            //     key: 8,
-            //     date: new Date('2022-5-1 00:00:00'),
-            //     title: 'One Second Everyday: April 2022',
-            //     videoContent: '',
-            //     type: 'One Second Everyday'
-            // },
-            // {
-            //     key: 9,
-            //     date: new Date('2022-6-1 00:00:00'),
-            //     title: 'One Second Everyday: May 2022',
-            //     videoContent: '',
-            //     type: 'One Second Everyday'
-            // },
-            {
-                key: 9,
-                date: new Date('2022-7-1 00:00:00'),
-                title: 'One Second Everyday: June 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565364/blog/one-second-everyday/jun-2022_ly0rnl.3gp',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 10,
-                date: new Date('2022-8-1 00:00:00'),
-                title: 'One Second Everyday: July 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670566194/blog/one-second-everyday/jul-2022_utn7lr.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 11,
-                date: new Date('2022-8-9 00:00:00'),
-                title: 'One Second Everyday: Mexico City #1 (August 2022)',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565370/blog/one-second-everyday/aug-2022-a_uembyh.3gp',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 12,
-                date: new Date('2022-8-14 00:00:00'),
-                title: 'One Second Everyday: Mexico City #2 (August 2022)',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565383/blog/one-second-everyday/aug-2022-b_ue7tlu.3gp',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 13,
-                date: new Date('2022-8-22 00:00:00'),
-                title: 'One Second Everyday: Taxco (August 2022)',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565252/blog/one-second-everyday/aug-2022-d_puczkn.3gp',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 14,
-                date: new Date('2022-8-31 00:00:00'),
-                title: 'One Second Everyday: Cholula (August 2022)',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565032/blog/one-second-everyday/aug-2022-c_heky3j.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 15,
-                date: new Date('2022-10-1 00:00:00'),
-                title: 'One Second Everyday: September 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565198/blog/one-second-everyday/sep-2022_osogjn.3gp',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 16,
-                date: new Date('2022-11-1 00:00:00'),
-                title: 'One Second Everyday: October 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565368/blog/one-second-everyday/oct-2022_j3u66e.mp4',
-                type: 'One Second Everyday'
-            },
-            {
-                key: 17,
-                date: new Date('2022-12-1 00:00:00'),
-                title: 'One Second Everyday: November 2022',
-                videoContent: 'https://res.cloudinary.com/svhoptoad/video/upload/v1670565364/blog/one-second-everyday/nov-2022_js7cir.mp4',
-                type: 'One Second Everyday'
-            },
-        ]
-
-    }
-
-
-    // const { title, subtitle, oneSecondEverydayVideos } = blog
+    const { title, subtitle, oneSecondEverydayVideos } = blog
     const newestOneSecondEveryDayVideoDate = oneSecondEverydayVideos.at(-1).date
     const newestBlogPost = new Date(blogPosts[0].date)
     const newestPost = newestOneSecondEveryDayVideoDate > newestBlogPost ? newestOneSecondEveryDayVideoDate : newestOneSecondEveryDayVideoDate
@@ -521,10 +388,6 @@ export async function getServerSideProps(context) {
           "photos": *[_type == "photo" && references(^._id)]{id, src, height, width, alt}
         }[0...24]
         `)
-
-
-
-
 
         return {
             props: {
